83c7c32377a7a5325ab5f2fa7d16ec6bb6a6d78a
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-06-12 14:42:11 +0300

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Write your code here!
+
 document.getElementById("main").remove();
 const newHeader = document.getElementById("victory");
-newHeader.innerText = "YOUR-NAME is the champion";
+// newHeader.innerText = "YOUR-NAME is the champion";
+const userName = (name)=>newHeader.innerText = `${name} is the champion`
+userName('Dennis')
